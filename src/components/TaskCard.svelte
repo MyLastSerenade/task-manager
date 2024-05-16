3516ed4c40c4dbe8d89d84f4b96b83105e96c221
MyLastSerenade <ssakanek86@googlemail.com>
2024-05-16 21:06:17 +0200

    tasks can now be deleted

--- src/components/TaskCard.svelte
+++ src/components/TaskCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { State, type Task } from '$lib/types/tasks';
+	import { State, tasks, type Task } from '$lib/types/tasks';
 	import Icon from './Icon.svelte';
 
 	let { ...props }: { task: Task } = $props();
@@ -17,18 +17,27 @@
 		}
 	}
 
-	function deleteTask() {
-		console.log("delete")
+	function deleteTask(id: number) {
+		let tasklist: Task[] = [];
+		tasks.subscribe((tasks) => {
+			tasklist = tasks;
+		});
+		let task = tasklist.find((task) => task.id == id) as Task;
+		if (task) {
+			tasklist.splice(tasklist.indexOf(task), tasklist.indexOf(task) + 1);
+			console.log(tasklist);
+		}
+		localStorage.setItem('tasks', JSON.stringify(tasklist));
 	}
 </script>
 
-<div class="w-full rounded-lg bg-secondary p-4 shadow-lg shadow-black">
+<div class="shadow-black w-full rounded-lg bg-secondary p-4 shadow-lg">
 	<div class="flex flex-row justify-between">
 		<h3>{props.task.title}</h3>
 		<p class={getTaskStateColor(props.task.taskState) + ' text-lg font-bold'}>
 			{props.task.taskState}
 		</p>
-		<Icon on:click={() => deleteTask()}>delete</Icon>
+		<Icon on:click={() => deleteTask(props.task.id)}>delete</Icon>
 	</div>
 	<p class="text-base font-normal">{props.task.content}</p>
 </div>
